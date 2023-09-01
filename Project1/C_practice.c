e3fe4ba85921ca1374f548fd3f3e25c7747ea37f
bumi <bumida456@naver.com>
2023-09-01 18:15:42 +0900

    hibi

--- Project1/C_practice.c
+++ Project1/C_practice.c
@@ -1,11 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
 	int a;
-	int bumida;
 	scanf("%d", &a);
-	print("�Է��� ���ڴ� %d�Դϴ�.\n", a);
 	system("pause");
 	return 0;
 }
